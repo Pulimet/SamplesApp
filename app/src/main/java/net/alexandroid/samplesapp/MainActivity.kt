9382df35493dd30f51a2f49231ae58214182070c
Pulimet <korolev2009@gmail.com>
2020-09-21 13:13:01 +0300

    Added a FragmentContainerView via main_activity.xml and removed redundant code in MainActivity

--- app/src/main/java/net/alexandroid/samplesapp/MainActivity.kt
+++ app/src/main/java/net/alexandroid/samplesapp/MainActivity.kt
@@ -1,17 +1,5 @@
 package net.alexandroid.samplesapp
 
-import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import net.alexandroid.samplesapp.ui.main.MainFragment
 
-class MainActivity : AppCompatActivity(R.layout.main_activity) {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.container, MainFragment.newInstance())
-                .commitNow()
-        }
-    }
-}
+class MainActivity : AppCompatActivity(R.layout.main_activity)
