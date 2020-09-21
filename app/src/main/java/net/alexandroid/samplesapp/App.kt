154efc0f06eee67054bad2756ce3a4a8288c61ac
Pulimet <korolev2009@gmail.com>
2020-09-21 13:46:40 +0300

    Added Logger library dependency and it settings in App.kt

--- app/src/main/java/net/alexandroid/samplesapp/App.kt
+++ app/src/main/java/net/alexandroid/samplesapp/App.kt
@@ -2,20 +2,26 @@ package net.alexandroid.samplesapp
 
 import android.app.Application
 import net.alexandroid.samplesapp.di.appModule
+import net.alexandroid.utils.mylogkt.MyLogKt
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
-import org.koin.core.logger.Level
 
-class App: Application() {
+class App : Application() {
 
     override fun onCreate() {
         super.onCreate()
+        setLogger()
         initKoin()
     }
 
+    private fun setLogger() {
+        MyLogKt.isLogsShown = BuildConfig.DEBUG
+        MyLogKt.tag = "@@@"
+    }
+
     private fun initKoin() {
-        startKoin{
+        startKoin {
             androidLogger()
             androidContext(this@App)
             modules(appModule)
