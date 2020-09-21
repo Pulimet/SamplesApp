4cfdc043c1e3080fc53273af531b1b6facfb9d24
Pulimet <korolev2009@gmail.com>
2020-09-21 12:00:36 +0300

    Initial commit

---
+++ app/src/main/java/net/alexandroid/samplesapp/MainActivity.kt
@@ -0,0 +1,18 @@
+package net.alexandroid.samplesapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import net.alexandroid.samplesapp.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }
+    }
+}
---
+++ app/src/main/java/net/alexandroid/samplesapp/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package net.alexandroid.samplesapp.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
