89be376bd54d6955e1c2f8a128ea200d3abc92d5
Pulimet <korolev2009@gmail.com>
2020-09-21 13:19:07 +0300

    MainFragment - Used 'by viewModels()' to get MainViewModel instance

--- app/src/main/java/net/alexandroid/samplesapp/ui/main/MainFragment.kt
+++ app/src/main/java/net/alexandroid/samplesapp/ui/main/MainFragment.kt
@@ -1,20 +1,9 @@
 package net.alexandroid.samplesapp.ui.main
 
-import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
+import androidx.fragment.app.viewModels
 import net.alexandroid.samplesapp.R
 
 class MainFragment : Fragment(R.layout.main_fragment) {
-
-    private lateinit var viewModel: MainViewModel
-
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
-    }
-
+    private val viewModel: MainViewModel by viewModels()
 }
