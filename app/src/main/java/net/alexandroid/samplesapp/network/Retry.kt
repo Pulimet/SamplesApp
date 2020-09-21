6c442a4720925a64cda5122dbf8b830d841d623b
Pulimet <korolev2009@gmail.com>
2020-09-21 16:17:28 +0300

    Added retry mechanism for networking solution

---
+++ app/src/main/java/net/alexandroid/samplesapp/network/Retry.kt
@@ -0,0 +1,40 @@
+package net.alexandroid.samplesapp.network
+
+import kotlinx.coroutines.delay
+import net.alexandroid.utils.mylogkt.logE
+
+sealed class NetworkResponse<T>
+class Success<T>(val data: T) : NetworkResponse<T>()
+class Fail<T>(val e: Exception) : NetworkResponse<T>()
+
+suspend fun <T> retryIO(
+    times: Int = 3,
+    initialDelay: Long = 500, // 1/2 second
+    maxDelay: Long = 4000,    // 4 seconds
+    factor: Double = 2.0,
+    desc: String = "",
+    block: suspend () -> T?
+): NetworkResponse<T> {
+    var currentDelay = initialDelay
+    var tempException: java.lang.Exception? = null
+    repeat(times) {
+        try {
+            val result = block()
+            if (result != null) return Success(result)
+        } catch (e: Exception) {
+            tempException = e
+            printLog(e, desc, it)
+        }
+
+        delay(currentDelay)
+        currentDelay = (currentDelay * factor).toLong().coerceAtMost(maxDelay)
+    }
+    return Fail(tempException ?: IllegalStateException("Unknown Exception"))
+}
+
+private fun printLog(e: Exception, desc: String, it: Int) {
+    logE(
+        "Description (${e.javaClass.simpleName}): $desc, fail counter: ${it + 1}, Exception: ${e.message} ",
+        t = e
+    )
+}
--- app/src/main/java/net/alexandroid/samplesapp/repo/MovieRepo.kt
+++ app/src/main/java/net/alexandroid/samplesapp/repo/MovieRepo.kt
@@ -2,6 +2,9 @@ package net.alexandroid.samplesapp.repo
 
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.flow
+import net.alexandroid.samplesapp.network.Fail
+import net.alexandroid.samplesapp.network.Success
+import net.alexandroid.samplesapp.network.retryIO
 import net.alexandroid.samplesapp.network.services.TmdbApiService
 
 class MovieRepo(private val tmdbApiService: TmdbApiService) {
@@ -10,7 +13,14 @@ class MovieRepo(private val tmdbApiService: TmdbApiService) {
         delay(1000)
         emit("We will show here a title of a movie")
         delay(1000)
-        emit(tmdbApiService.getMovies().results[0].title)
+
+        val result = retryIO(desc = "Get movies") {
+            tmdbApiService.getMovies()
+        }
+        when (result) {
+            is Success -> emit(result.data.results[0].title)
+            is Fail -> emit(result.e.message?: "Failed")
+        }
     }
 
 }
