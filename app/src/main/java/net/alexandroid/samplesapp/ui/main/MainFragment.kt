4b7a4163836c3c8786aa1916f2eddfc85a4a72f6
Pulimet <korolev2009@gmail.com>
2020-09-21 15:00:21 +0300

    Add to the app module gradle plugin  'kotlin-android-extensions' that allows synthetic usage (Access views references without explicit findViewById) and prepare a TextView for movie title.

--- app/src/main/java/net/alexandroid/samplesapp/ui/main/MainFragment.kt
+++ app/src/main/java/net/alexandroid/samplesapp/ui/main/MainFragment.kt
@@ -1,9 +1,17 @@
 package net.alexandroid.samplesapp.ui.main
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.main_fragment.*
 import net.alexandroid.samplesapp.R
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainFragment : Fragment(R.layout.main_fragment) {
     private val viewModel: MainViewModel by viewModel()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        tvVideoTitle.text = "We will show here a title of movie"
+    }
 }
