09fcb4133f1e91befbef0d256630d19559cb9b18
Pulimet <korolev2009@gmail.com>
2020-09-21 13:55:35 +0300

    Added MainViewModel to the Koin's module list and in MainFragment.kt used Koin to get MainViewModel reference

--- app/src/main/java/net/alexandroid/samplesapp/di/AppModule.kt
+++ app/src/main/java/net/alexandroid/samplesapp/di/AppModule.kt
@@ -1,7 +1,12 @@
 package net.alexandroid.samplesapp.di
 
+import net.alexandroid.samplesapp.ui.main.MainViewModel
+import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val appModule = module {
+
+    // ViewModels
+    viewModel { MainViewModel() }
 
 }
--- app/src/main/java/net/alexandroid/samplesapp/ui/main/MainFragment.kt
+++ app/src/main/java/net/alexandroid/samplesapp/ui/main/MainFragment.kt
@@ -1,9 +1,9 @@
 package net.alexandroid.samplesapp.ui.main
 
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.viewModels
 import net.alexandroid.samplesapp.R
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainFragment : Fragment(R.layout.main_fragment) {
-    private val viewModel: MainViewModel by viewModels()
+    private val viewModel: MainViewModel by viewModel()
 }
