5d78f71be48c48650d2467f45c8ab83329fa535f
Pulimet <korolev2009@gmail.com>
2020-09-21 14:16:32 +0300

    Init koin not on the main thread using coroutines and print time it took to init Koin.

--- app/src/main/java/net/alexandroid/samplesapp/App.kt
+++ app/src/main/java/net/alexandroid/samplesapp/App.kt
@@ -1,14 +1,21 @@
 package net.alexandroid.samplesapp
 
 import android.app.Application
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import net.alexandroid.samplesapp.di.KoinLogs
 import net.alexandroid.samplesapp.di.appModule
 import net.alexandroid.utils.mylogkt.MyLogKt
+import net.alexandroid.utils.mylogkt.info
 import net.alexandroid.utils.mylogkt.warn
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
+import kotlin.coroutines.CoroutineContext
+import kotlin.system.measureTimeMillis
 
-class App : Application() {
+class App : Application(), CoroutineScope {
+    override val coroutineContext: CoroutineContext = Dispatchers.IO
 
     override fun onCreate() {
         super.onCreate()
@@ -22,11 +29,14 @@ class App : Application() {
         warn("=== App created and logger is ready ===")
     }
 
-    private fun initKoin() {
-        startKoin {
-            KoinLogs()
-            androidContext(this@App)
-            modules(appModule)
+    private fun initKoin() = launch {
+        val koinInitTime = measureTimeMillis {
+            startKoin {
+                KoinLogs()
+                androidContext(this@App)
+                modules(appModule)
+            }
         }
+        info("=== Koin init time: $koinInitTime ms ===")
     }
 }
