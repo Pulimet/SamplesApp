9776cbd555a0e6fb430fd38ba94f4ce58762447a
Pulimet <korolev2009@gmail.com>
2020-09-21 13:15:40 +0300

    Added MainFragment to nav_graph.xml and removed redundant code from MainFragment

--- app/src/main/java/net/alexandroid/samplesapp/ui/main/MainFragment.kt
+++ app/src/main/java/net/alexandroid/samplesapp/ui/main/MainFragment.kt
@@ -10,16 +10,11 @@ import net.alexandroid.samplesapp.R
 
 class MainFragment : Fragment(R.layout.main_fragment) {
 
-    companion object {
-        fun newInstance() = MainFragment()
-    }
-
     private lateinit var viewModel: MainViewModel
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
-        // TODO: Use the ViewModel
     }
 
 }
