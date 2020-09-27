fac09fdc66c971a2db05fd2805f9f820c48af232
Pulimet <korolev2009@gmail.com>
2020-09-27 16:12:45 +0300

    Updated gradle and added map transformation example

--- app/src/main/java/net/alexandroid/samplesapp/ui/main/MainViewModel.kt
+++ app/src/main/java/net/alexandroid/samplesapp/ui/main/MainViewModel.kt
@@ -2,6 +2,7 @@ package net.alexandroid.samplesapp.ui.main
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.asLiveData
+import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.onStart
 import net.alexandroid.samplesapp.repo.MovieRepo
 import net.alexandroid.samplesapp.ui.BaseViewModel
@@ -11,6 +12,7 @@ class MainViewModel(private val movieRepo: MovieRepo) : BaseViewModel() {
 
     fun getMovieTitle(): LiveData<String> = movieRepo.getMovieTitle()
         .onStart { emit("Loading...") }
+        .map { "Movie: $it" }
         .asLiveData()
 
 }
