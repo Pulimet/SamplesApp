5edca896a4a8603b73bf3377d03a2312f550b5e2
Pulimet <korolev2009@gmail.com>
2020-09-28 03:54:01 +0300

    DataStore usage example (DataStoreFragment.kt, DataStoreViewModel.kt)

--- app/src/main/java/net/alexandroid/samplesapp/di/AppModule.kt
+++ app/src/main/java/net/alexandroid/samplesapp/di/AppModule.kt
@@ -1,11 +1,15 @@
 package net.alexandroid.samplesapp.di
 
+import androidx.datastore.DataStore
+import androidx.datastore.preferences.Preferences
+import androidx.datastore.preferences.createDataStore
 import androidx.room.Room
 import net.alexandroid.samplesapp.db.MovieDatabase
 import net.alexandroid.samplesapp.network.NetworkObjectsCreator
 import net.alexandroid.samplesapp.network.services.TmdbApiService
 import net.alexandroid.samplesapp.repo.MovieRepo
 import net.alexandroid.samplesapp.ui.NavigationViewModel
+import net.alexandroid.samplesapp.ui.datastore.DataStoreViewModel
 import net.alexandroid.samplesapp.ui.main.MainViewModel
 import org.koin.android.ext.koin.androidContext
 import org.koin.androidx.viewmodel.dsl.viewModel
@@ -14,6 +18,9 @@ import org.koin.dsl.module
 const val TMDB_URL = "https://api.themoviedb.org/"
 
 val appModule = module {
+
+    // DataStore
+    single<DataStore<Preferences>> { androidContext().createDataStore("storage") }
 
     // DB
     single { Room.databaseBuilder(get(), MovieDatabase::class.java, "movies_database").build() }
@@ -28,6 +35,7 @@ val appModule = module {
 
     // ViewModels
     viewModel { MainViewModel(get()) }
+    viewModel { DataStoreViewModel(get()) }
     viewModel { NavigationViewModel() }
 
 }
--- app/src/main/java/net/alexandroid/samplesapp/ui/datastore/DataStoreFragment.kt
+++ app/src/main/java/net/alexandroid/samplesapp/ui/datastore/DataStoreFragment.kt
@@ -1,9 +1,35 @@
 package net.alexandroid.samplesapp.ui.datastore
 
+import android.os.Bundle
+import android.view.View
+import androidx.lifecycle.lifecycleScope
+import kotlinx.android.synthetic.main.fragment_data_store.*
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 import net.alexandroid.samplesapp.R
 import net.alexandroid.samplesapp.utils.BaseFragment
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
-class DataStoreFragment : BaseFragment(R.layout.fragment_data_store) {
+class DataStoreFragment : BaseFragment(R.layout.fragment_data_store), View.OnClickListener {
 
+    private val viewModel: DataStoreViewModel by viewModel()
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        lifecycleScope.launch {
+            viewModel.userPreferencesFlow.collect {
+                tvResult.text = it
+            }
+        }
+
+        btnSave.setOnClickListener(this)
+    }
+
+    // View.OnClickListener
+    override fun onClick(v: View?) {
+        when (v) {
+            btnSave -> viewModel.onBtnSaveClick(etText.text.toString())
+        }
+    }
 }
